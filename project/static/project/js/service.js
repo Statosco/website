52adb44b86eac901ba25c6bcf0a86534851175a3
solomon jesse <statosco@gmail.com>
2024-06-18 13:28:14 +0300

    important info added

--- project/static/project/js/service.js
+++ project/static/project/js/service.js
@@ -205,30 +205,18 @@ window.addEventListener('scroll', hideAllContents);
 
 
 
-const readMoreBtn1 = document.getElementById("read-more-1");
-const heroPara1 = document.getElementById("hero-para-1");
-const closeBtn1 = heroPara1.querySelector(".close");
-
-readMoreBtn1.addEventListener("click", function() {
-  heroPara1.classList.remove("hidden");
+document.body.addEventListener("click", function(event) {
+  if (event.target.classList.contains("read-more")) {
+    const targetId = event.target.getAttribute("data-target");
+    const heroPara = document.getElementById(targetId);
+    heroPara.classList.remove("hidden");
+  }
+  
+  if (event.target.classList.contains("close")) {
+    const heroPara = event.target.closest(".hero-para");
+    heroPara.classList.add("hidden");
+  }
 });
-
-closeBtn1.addEventListener("click", function() {
-  heroPara1.classList.add("hidden");
-});
-
-const readMoreBtn2 = document.getElementById("read-more-2");
-const heroPara2 = document.getElementById("hero-para-2");
-const closeBtn2 = heroPara2.querySelector(".close");
-
-readMoreBtn2.addEventListener("click", function() {
-  heroPara2.classList.remove("hidden");
-});
-
-closeBtn2.addEventListener("click", function() {
-  heroPara2.classList.add("hidden");
-});
-
 
 
 });
