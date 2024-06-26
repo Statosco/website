b168f086013937473c8c1e2b7d29d200b4159d0f
solomon jesse <statosco@gmail.com>
2024-06-26 17:27:17 +0300

    socials

--- project/views.py
+++ project/views.py
@@ -10,6 +10,22 @@ def about(response):
     return render(response, 'project/about.html')
 
 
+def socials(response):
+    return render(response, 'project/socials.html')
+
+
+from django.http import FileResponse, Http404
+
+import os
+from django.conf import settings
+
+def download_cv(request):
+    file_path = os.path.join(settings.BASE_DIR, 'project', 'static', 'project', 'files', 'SolomonJessecv.pdf')
+    if os.path.exists(file_path):
+        return FileResponse(open(file_path, 'rb'), as_attachment=True, filename='SolomonJessecv.pdf')
+    else:
+        raise Http404("CV not found")
+
 # views.py
 from django.shortcuts import render, redirect
 from django.core.mail import EmailMessage
